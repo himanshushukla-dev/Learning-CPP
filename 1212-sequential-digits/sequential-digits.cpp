5e249a8bf84ebe35fbd8d4914e430392fd50c9ae
Himanshu Shukla <himanshushukla0018@gmail.com>
2026-07-13 20:47:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (48.96%) - LeetSync

---
+++ 1212-sequential-digits/sequential-digits.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> sequentialDigits(int low, int high) 
+    {
+        vector<int> result;
+        string digits = "123456789";
+        for (int length = 2; length <= 9; ++length) 
+        {
+            for (int start = 0; start <= 9 - length; ++start) 
+            {
+                string sub = digits.substr(start, length);
+                int num = stoi(sub);
+                if (num >= low && num <= high) 
+                {
+                    result.push_back(num);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
