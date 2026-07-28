1b76a652910f95969888dd60168bede226ff99be
Himanshu Shukla <himanshushukla0018@gmail.com>
2026-07-28 08:13:52 +0530

    Time: 59 ms (46.15%) | Memory: 69.3 MB (69.23%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string smallestPalindrome(string s) 
+    {
+        int n = s.length();
+        
+        string half = s.substr(0, n / 2);
+        
+        sort(half.begin(), half.end());
+        
+        string rev_half = half;
+        reverse(rev_half.begin(), rev_half.end());
+        
+        if (n % 2 == 1) 
+        {
+            return half + s[n / 2] + rev_half;
+        } 
+        else 
+        {
+            return half + rev_half;
+        }
+    }
+};
