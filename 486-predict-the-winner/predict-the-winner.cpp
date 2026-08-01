c48161dda2e9e81b65baece81d5773fabdb52047
Himanshu Shukla <himanshushukla0018@gmail.com>
2026-08-01 10:02:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (69.71%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    bool predictTheWinner(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp = nums;
+
+        for (int length = 2; length <= n; ++length) 
+        {
+            for (int i = 0; i <= n - length; ++i) 
+            {
+                int j = i + length - 1;
+                dp[i] = max(nums[i] - dp[i + 1], nums[j] - dp[i]);
+            }
+        }
+
+        return dp[0] >= 0;
+    }
+};
